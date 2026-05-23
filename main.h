51dc88f2b75d6bb890a6ce8756d6c9f3221ad3f0
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-05-23 21:41:01 +0930

    Atomic Ring Buffer definitions

--- main.h
+++ main.h
@@ -1,12 +1,14 @@
-#include <stdlib.h>
+#include <stdatomic.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
+// RING BUFFER
 const int BUFFER_SIZE = 5;
 typedef struct RingBuff {
-    char *read_ptr;
-    char *write_ptr;
-    char arr[BUFFER_SIZE];
-    bool full;
+  char *read_ptr;
+  char *write_ptr;
+  char arr[BUFFER_SIZE];
+  bool full;
 } RingBuff;
 
 RingBuff *init();
@@ -15,3 +17,17 @@ void freeRB(RingBuff *rb);
 int spaceLeft(RingBuff *rb);
 void write(RingBuff *rb, char newChar);
 char read(RingBuff *rb);
+
+// ATOMIC RING BUFFER
+typedef struct {
+  char arr[BUFFER_SIZE];
+  atomic_size_t head;
+  atomic_size_t tail;
+} AtomicRingBuff;
+
+AtomicRingBuff *initARB();
+void resetARB(AtomicRingBuff *rb);
+void freeARB(AtomicRingBuff *rb);
+int spaceLeftARB(AtomicRingBuff *rb);
+void writeARB(AtomicRingBuff *rb, char newChar);
+char readARB(AtomicRingBuff *rb);
